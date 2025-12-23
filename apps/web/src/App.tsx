adb2e9fcabcecca1f6ad13c151a0963a6f581a92
Desmond Brown <browndesmond30@yahoo.com>
2025-12-23 09:40:07 +0000

    feat: added setup for vite+react+shadcn/ui

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+    return <div className="text-purple-600 text-2xl font-bold">
+      Tailwind is working 
+    </div>
+}
+
+export default App
