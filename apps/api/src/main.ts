69db7a00c881b14a2de3e5862b4ff92986bea12c
Desmond Brown <browndesmond30@yahoo.com>
2025-12-23 13:27:49 +0000

    feat: replaced NestJS http serving with Fasify

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, 
+    new FastifyAdapter({logger: true})
+  );
+  app.enableCors({origin: true, credentials: true});
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    })
+  )
 }
 bootstrap();
+
